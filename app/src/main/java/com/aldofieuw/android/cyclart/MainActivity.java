256ea55e76141157e15e5fdf8b5d8394f2605075
Aldo <aldofieuw@hotmail.com>
2017-03-26 15:15:38 +0200

    Initial commit

---
+++ app/src/main/java/com/aldofieuw/android/cyclart/MainActivity.java
@@ -0,0 +1,18 @@
+package com.aldofieuw.android.cyclart;
+
+import android.media.session.MediaController;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//import static com.aldofieuw.android.cyclart.R.id.videoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
